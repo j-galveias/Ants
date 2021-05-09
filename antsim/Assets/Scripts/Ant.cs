0a4569318dac2e8000f746e61504bdaa2b8bfc8a
j-galveias <brainsuprajr@gmail.com>
2021-05-09 18:00:43 +0100

    first commit

---
+++ antsim/Assets/Scripts/Ant.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ant : MonoBehaviour
+{
+    public float maxSpeed = 2;
+    public float steerStrength = 2;
+    public float wanderStrength = 1;
+
+    Vector2 position;
+    Vector2 velocity;
+    Vector2 desiredDirection;
+
+    // Update is called once per frame
+    void Update()
+    {
+        desiredDirection = (desiredDirection + Random.insideUnitCircle * wanderStrength).normalized;
+
+        Vector2 desiredVelocity = desiredDirection * maxSpeed;
+        Vector2 desiredSteeringForce = (desiredVelocity - velocity) * steerStrength;
+        Vector2 acceleration = Vector2.ClampMagnitude(desiredSteeringForce, steerStrength) / 1;
+
+        velocity = Vector2.ClampMagnitude(velocity + acceleration * Time.deltaTime, maxSpeed);
+        position += velocity * Time.deltaTime;
+
+        float angle = Mathf.Atan2(velocity.y, velocity.x) * Mathf.Rad2Deg;
+        transform.SetPositionAndRotation(position, Quaternion.Euler(0, 0, angle));
+    }
+}
